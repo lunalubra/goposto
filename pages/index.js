8c2e39360035a62d005f1143416866850a801dc0
Lucas Acosta <lucas2002lucas@gmail.com>
2021-05-10 15:30:08 +0200

    Updated the ui

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,26 @@
-import {Flex, Link, Text} from "@chakra-ui/layout";
-import {EditIcon} from "@chakra-ui/icons";
-import {Button} from "@chakra-ui/button";
-import NextLink from "next/link";
-import Image from "next/image";
-import Head from "next/head";
+import {
+  Flex,
+  Container,
+  Heading,
+  Stack,
+  Text,
+  Button
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
-import GoogleButton from "@/components/GoogleButton";
-import {useAuth} from "@/lib/auth";
-import WelcomeCard from "@/components/WelcomeCard";
+import { Illustration } from '@/components/Ilustration';
+import GoogleButton from '@/components/GoogleButton';
+import { useAuth } from '@/lib/auth';
 
-export default function Home() {
-  const {user, loading, signinWithGoogle} = useAuth();
+export default function CallToActionWithIllustration() {
+  const { user, loading, signinWithGoogle } = useAuth();
+  const router = useRouter();
 
   return (
     <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              if (document.cookie && document.cookie.includes('invoice-app-auth')) {
-                window.location.href = "/dashboard"
-              }
-            `,
-          }}
-        />
-      </Head>
-      <Flex bg="white" position="relative" direction="column" minW="100vw">
-        <Flex minW="100vw" p={2} justifyContent="space-between">
+      <Flex minW="100vw" alignItems="center" justifyContent="center" bg="white">
+        <Flex w="100%" maxW="1200px" p={2} justifyContent="space-between">
           <Image src="/posto.svg" width={80} height={20} alt="logo" />
           {loading ? (
             <Text>Loading</Text>
@@ -40,39 +34,52 @@ export default function Home() {
             <GoogleButton callback={signinWithGoogle} />
           )}
         </Flex>
-        <Flex alignItems="center" mt={["96px"]} direction="column">
-          <Text
-            bgGradient="linear(to-l, #7928CA, #1262ff)"
-            bgClip="text"
-            fontSize={"5xl"}
-            fontWeight="extrabold"
-            lineHeight={"60px"}
-            textAlign="center"
-          >
-            Go the easy way.
-          </Text>
-          <Text
-            mt={["20px"]}
-            fontSize="xl"
-            fontWeight="semibold"
-            textAlign="center"
-            pb="96px"
-          >
-            Posto is a new way of handeling invoices & clients for freelancers
-          </Text>
-        </Flex>
       </Flex>
-      <Flex
-        marginY={10}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <WelcomeCard />
-        <GoogleButton mt={5} icon={<EditIcon />} callback={signinWithGoogle}>
-          Try it for free!
-        </GoogleButton>
-      </Flex>
+      <Container maxW={'4xl'} mt="96px">
+        <Stack
+          textAlign={'center'}
+          align={'center'}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}
+        >
+          <Heading fontWeight="extrabold" fontSize="6xl" lineHeight={'110%'}>
+            Handleling clients{' '}
+            <Text as={'span'} color={'brand.400'}>
+              made easy
+            </Text>
+          </Heading>
+          <Text color={'gray.500'} maxW={'3xl'} mt="48px">
+            posto is the new way of handleling clients and invoices for
+            freelancer. Never has been this easy to keep your client list and
+            invoices in one place, organiced and structured, and even recive
+            analytics!
+          </Text>
+          <Stack spacing={6} direction={'row'} my="48px">
+            <Button
+              rounded={'full'}
+              px={6}
+              colorScheme={'brand.400'}
+              bg={'brand.400'}
+              _hover={{ opacity: '0.6' }}
+              onClick={async (e) => {
+                await signinWithGoogle();
+                router.push(`/dashboard`);
+              }}
+            >
+              Get started
+            </Button>
+            <Button rounded={'full'} px={6}>
+              Learn more
+            </Button>
+          </Stack>
+          <Flex w={'full'}>
+            <Illustration
+              height={{ sm: '24rem', lg: '28rem' }}
+              mt={{ base: 12, sm: 16 }}
+            />
+          </Flex>
+        </Stack>
+      </Container>
     </>
   );
 }
